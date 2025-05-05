5422fdcdb184960cf3a6cdf70e8875d304816acb
Your Name <you@example.com>
2025-05-05 13:26:32 +0800

    modified:   Student_Perf/Preprocessing.py new file:   Testing/Student_Habit_Performance.ipynb

--- Student_Perf/Preprocessing.py
+++ Student_Perf/Preprocessing.py
@@ -1,4 +1,3 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 df = pd.read_csv(r"c:\Users\Nelson's lappy\OneDrive\Desktop\student_habits_performance.csv")
-print(df.isnull().sum())
